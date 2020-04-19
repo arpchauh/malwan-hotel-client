261287547bab342af2446800cd4328d8f0e9996a
arpchauh <arpitchauhan10@gmail.com>
2020-04-19 18:28:49 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HotelDescriptionComponent } from "./home/hotel-description/hotel-description.component";
-import { RoomsComponent } from "./home/rooms/rooms.component";
-import { HeaderComponent } from "./home/header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./admin/login/login.component";
 import { AdminHomeComponent } from "./admin/admin-home/admin-home.component";
-import { AdminComponent } from "./admin/admin.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   {
     path: "admin",
-    component: HomeComponent,
+    component: LoginComponent,
     children: [
-      {
-        path: "home",
-        component: AdminHomeComponent,
-      },
       {
         path: "login",
         component: LoginComponent,
+      },
+      {
+        path: "home",
+        component: AdminHomeComponent,
       },
     ],
   },
